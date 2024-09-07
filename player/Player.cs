0938b667f4b50f3de6d995729fc422acb1cfc938
Flavio Assis <flavioh.dev@gmail.com>
2024-09-06 23:18:58 -0300

    Change Player 1 color and clean the code

--- player/Player.cs
+++ player/Player.cs
@@ -2,18 +2,9 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	int IdPlayer = 1;
 	const float Speed = 300f;
-
-	string upInput;
-	string downInput;
-
-	public override void _Ready()
-	{
-		upInput = $"UP_{IdPlayer}";
-		downInput = $"DOWN_{IdPlayer}";
-	}
+	const string UpInput = $"UP_1";
+	const string DownInput = $"DOWN_1";
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -21,15 +12,12 @@ public partial class Player : CharacterBody2D
 		MoveAndCollide(this.Velocity * (float)delta);
 	}
 
-	private Vector2 GetVelocityFromInput()
+	private static Vector2 GetVelocityFromInput()
 	{
-		var ballPosition = GetNode<KinematicCollision2D>("Ball").GlobalPosition;
-
-
-		if (Input.IsActionPressed(upInput))
+		if (Input.IsActionPressed(UpInput))
 			return new Vector2(0, -Speed);
 
-		if (Input.IsActionPressed(downInput))
+		if (Input.IsActionPressed(DownInput))
 			return new Vector2(0, Speed);
 
 		return new Vector2(0, 0);
